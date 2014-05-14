7bc761af807300d2dee85ecbbbb310f7d839a146
hello <15901423703@63.com>
2014-05-14 10:18:32 +0800

    my first try commit

---
+++ MyFirst/MyFirst/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyFirst
+//
+//  Created by 鸿妈 on 14-5-14.
+//  Copyright (c) 2014年 Naomi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
